20061a82d403b262f04644164a42ad9aa0a40155
Kedar Naik <kedarnaik515@gmail.com>
2018-05-24 10:50:32 +0530

    My first Commit

---
+++ .dep/GPS_TRACKER.o.d
@@ -0,0 +1,5 @@
+GPS_TRACKER.o: GPS_TRACKER.c serial.h lcd.h
+
+serial.h:
+
+lcd.h:
---
+++ GPS_TRACKER.c
@@ -0,0 +1,107 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+///  Project Name : GPS_TRACKER
+//
+//  Target Board : atmega32
+//
+//  Date Created : 8/4/2017 10:09:02 AM
+// 
+//  Online Resource  : www.electronicsforstudents.com
+//
+//  Libraries Included : -->gprs -->gps -->gsm -->power_reset 
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<avr/io.h>
+#include<util/delay.h>
+
+
+#include "serial.h"
+#include "lcd.h"
+
+
+void beep()
+{
+	write(porta,3,h);
+	_delay_ms(300);
+	write(porta,3,l);
+}
+
+int main()
+{
+
+	beep();
+	char url[120],response[10];
+
+	serial_init(9600);
+	serial_select(3);
+
+	lcd_init();
+
+	lcd_string_P(PSTR("GPS Tracker"));
+
+		set_timeout(30);	//Auto Reset If code gets blocked for more than 30 seconds
+
+	gsm_init();
+	
+		reset_timeout();
+
+	gprs_init();   
+	
+		reset_timeout();
+
+	agps_init();
+
+	//gps_init();
+	
+		reset_timeout();
+
+	gprs_connect();
+	
+		reset_timeout();
+
+	char lat[10];
+	char lon[10];
+	char speed[10];
+
+	while (1)
+	{
+           		
+
+		if(gps_fix()==1)
+		{
+		
+			reset_timeout();
+			
+			gps_getcoordinates(lat,lon,speed);
+		  
+			lcd_clear();
+			lcd_string(lat);
+			lcd_gotoxy(0,1);
+			lcd_string(lon);
+			
+			reset_timeout();
+		
+			sprintf(url,"api.clobous.com/KED515/track?&lat=%s&long=%s&speed=%s",lat,lon,speed);
+		 
+			http_get(url,response);
+			
+			reset_timeout();
+			
+				lcd_clear();
+				lcd_string(response);
+				_delay_ms(1000);
+		}
+		
+		else{
+				lcd_clear();
+				lcd_string("NO GPS FIX");
+				_delay_ms(50);
+			 }
+
+
+		_delay_ms(500);
+
+
+
+	}
+
+}
